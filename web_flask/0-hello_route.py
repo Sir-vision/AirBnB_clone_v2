59dd3ae0c6133bd4ff25b0aa998bc4036fa1152c
Oyebode Adetunji <adetunji.oyebode6@gmail.com>
2023-04-23 23:31:24 +0100

    Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """Flask"""
 from flask import Flask
-app = Flask(__name__)
 
+
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_route():
@@ -10,4 +11,4 @@ def hello_route():
     return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
