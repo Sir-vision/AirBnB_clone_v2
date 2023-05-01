c5578ac4af312e2bfdd204e2d2f0e9de17c01232
Oyebode Adetunji <adetunji.oyebode6@gmail.com>
2023-05-01 11:59:40 +0000

    Console improvements

--- models/amenity.py
+++ models/amenity.py
@@ -1,20 +1,7 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-import os
+""" State Module for HBNB project """
+from models.base_model import BaseModel
 
 
-class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-
-    if os.getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        place_amenities = relationship("Place", secondary="place_amenity")
-    else:
-        name = ""
+class Amenity(BaseModel):
+    name = ""
